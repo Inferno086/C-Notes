a4ad9ad8c369b2bceea967b3042337e45c9fcaea
Inferno086 <93240321+Inferno086@users.noreply.github.com>
2022-03-27 18:46:06 +0530

    Add files via upload

---
+++ 3-variables_and_comments.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*A comment is a human-readable text in the
+    source code, which is ignored by the compiler.*/
+
+    // Variables are containers to store our data
+    /*Variable names in C++ language can range from 1 to 255
+    Variable names must start with a letter of the alphabet or an underscore
+    After the first letter, variable names can contain letters and numbers
+    Variable names are case sensitive
+    No spaces and special characters are allowed
+    We cannot use reserved keywords as a variable name*/
+
+    /*Reserved keywords are those keywords that are used by the language
+    itself, which is why these keywords are not available for re-definition
+     or overloading. In short, you cannot create variables with these names.*/
+    int sum = 50;
+    cout<<"The sum is "<<sum;
+    return 0;
+}
